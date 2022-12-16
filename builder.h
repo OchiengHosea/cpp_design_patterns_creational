478a5c58f93b60ba3cf68af1e76d612d1d87bf9c
OchiengHosea <jrhosea8@gmail.com>
2022-12-16 17:33:20 +0300

    docs: added comments

--- builder.h
+++ builder.h
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <string>
 
+// forward declare class to avoid compilation errors
 class CoffeeBuilder;
 
 class Coffee {
@@ -19,7 +20,7 @@ public:
     bool cost;
 
     Coffee(std::string requestorName) : requestorName(requestorName) {}
-    friend class CoffeeBuilder;
+    friend class CoffeeBuilder; // allows coffee builder to peek into internals of Coffee
     static CoffeeBuilder create(std::string requestorName);
 
 };
@@ -30,6 +31,7 @@ class CoffeeBuilder{
 public:
     CoffeeBuilder(std::string requestorName) : coffee(Coffee(requestorName)) {}
 
+    // operator that allows conversion between coffee builder to coffee class
     operator Coffee() const { return std::move(coffee); }
 
     CoffeeBuilder& makeHot();
